f84a84230af0c2a46b6edf63e771341a8fc7aead
adrcho <43699884+adrcho@users.noreply.github.com>
2020-04-10 15:53:22 +0200

    Update trigger.c

--- trigger.c
+++ trigger.c
@@ -36,8 +36,9 @@ const double full_sweep_time=100e-6; //s  <125us*
 //Buffer depth 
 
 const int BUF = 16*1024;
-const int decimation = 1;    						// decimation: [1;8;64;1024;8192;65536]
-const int N = full_sweep_time*125e6/decimation; 			// desired length of trace (1,..., 16383)
+const int decimation = 1;    // decimation: [1;8;64;1024;8192;65536]
+int test = full_sweep_time*125e6/decimation; 
+const int N = test;			// desired length of trace (1,..., 16383)
 	// decimation: [1;8;64;1024;8192;65536]
 
 int main(void) 
