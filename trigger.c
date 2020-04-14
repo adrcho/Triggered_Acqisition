eb73d991c043ccee775025d8791800793136d8e4
adrcho <43699884+adrcho@users.noreply.github.com>
2020-04-14 15:53:59 +0200

    Update trigger.c

--- trigger.c
+++ trigger.c
@@ -243,9 +243,9 @@ int main(void)
 	    /*now read N samples from the trigger pointer location.*/
 	    int i;
 	    int data[N]={ };  // sure its an int?
-	    for (i=0; i < N; i+=2)
+	    for (i=0; i < N; i+=1)
 	    {
-		memcpy(&data[i], &cha_signal[(trig_ptr+i)%BUF], 2*sizeof(int));
+		memcpy(&data[i], &cha_signal[(trig_ptr+i)%BUF], 1*sizeof(int));
 	    }
 	    
 	    //for (i=0; i < N; i+=1)
