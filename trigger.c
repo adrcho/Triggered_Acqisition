6900bbc4eed5fd3ee37bffda3bd1f746675ede3f
adrcho <43699884+adrcho@users.noreply.github.com>
2020-04-15 16:06:00 +0200

    Update trigger.c

--- trigger.c
+++ trigger.c
@@ -239,13 +239,13 @@ int main(void)
 	//----------------------------------------//
 	// same speed for writing in a file.
 			
-	 //  /*now read N samples from the trigger pointer location.*/
-	 //  int i;
-	//  int data[N]={ };  // sure its an int?
-	//   for (i=0; i < N; i++)
-	//  {
-	//	memcpy(&data[i], &cha_signal[(trig_ptr+i)%BUF], 1*sizeof(int));
-	  //  }
+	   /*now read N samples from the trigger pointer location.*/
+	  int i;
+	  int data[N]={ };  // sure its an int?
+	   for (i=0; i < N; i++)
+	  {
+		memcpy(&data[i], &cha_signal[(trig_ptr+i)%BUF], 1*sizeof(int));
+	   }
 	    
 	    //for (i=0; i < N; i+=1)
 	    //{
@@ -253,19 +253,7 @@ int main(void)
 	    //}
 		
 		
-	//----------------------------------------//
-	//-----------No Data processing-----------//
-	//-------------Negative nalues------------//
-	//----------------------------------------//
-	//----------------------------------------//
-	//-------Full_values_collection-------//
-	//----------------------------------------//	
-	
-	//  /*now read N samples from the trigger pointer location.*/
-	//  int i;
-        int data[N]={ };  // sure its an int?	       
-	memcpy(&data, &cha_signal[(trig_ptr)%BUF], 1*sizeof(int));  
-		       
+ 
 	    loop_t2=clock();
 	    loop_t = (long double)(loop_t2 - loop_t1);
 	    printf("Ellapsed time %ld clock cycles\n" ,loop_t);
